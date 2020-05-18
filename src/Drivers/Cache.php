ded822b8f4362d5468d54543ce7883e77ea2e4d5
codesinging <codesinging@gmail.com>
2020-05-18 21:15:33 +0800

    Add comments in the file header

--- src/Drivers/Cache.php
+++ src/Drivers/Cache.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * Author:  CodeSinging (The code is singing)
+ * Email:   codesinging@gmail.com
+ * Github:  https://github.com/codesinging
+ * Time:    2020-05-18 21:13:30
+ */
 
 namespace CodeSinging\ThinkSae\Drivers;
 
--- src/config.php
+++ src/config.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Author:  CodeSinging (The code is singing)
+ * Email:   codesinging@gmail.com
+ * Github:  https://github.com/codesinging
+ * Time:    2020-05-18 21:13:30
+ */
+
 return [
     // Cache
     'cache' => [
